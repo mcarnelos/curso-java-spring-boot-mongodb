c81da0a72a92630c7367cbbc12f9ec63d09c4db9
Murilo Carnelos <muriloc.c@hotmail.com>
2020-04-15 17:04:08 -0300

    Obtendo um usuário por id

--- src/main/java/com/murilocarnelos/workshopmongodb/services/UserService.java
+++ src/main/java/com/murilocarnelos/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.murilocarnelos.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.murilocarnelos.workshopmongodb.domain.User;
 import com.murilocarnelos.workshopmongodb.repository.UserRepository;
+import com.murilocarnelos.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,12 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
 }
+
+
