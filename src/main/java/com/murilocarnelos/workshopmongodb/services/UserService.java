dd2143621902045ec5057f0c3becefc74e2b6130
Murilo Carnelos <muriloc.c@hotmail.com>
2020-04-15 15:44:00 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/murilocarnelos/workshopmongodb/services/UserService.java
@@ -0,0 +1,21 @@
+package com.murilocarnelos.workshopmongodb.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.murilocarnelos.workshopmongodb.domain.User;
+import com.murilocarnelos.workshopmongodb.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
