6e4c48c775a0382318104673049a9271bc9376b1
Murilo Carnelos <muriloc.c@hotmail.com>
2020-04-16 16:31:29 -0300

    Obtendo um post por id

---
+++ src/main/java/com/murilocarnelos/workshopmongodb/services/PostService.java
@@ -0,0 +1,27 @@
+package com.murilocarnelos.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.murilocarnelos.workshopmongodb.domain.Post;
+import com.murilocarnelos.workshopmongodb.repository.PostRepository;
+import com.murilocarnelos.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+	
+	
+}
+
+
