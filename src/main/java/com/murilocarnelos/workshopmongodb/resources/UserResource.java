9cf98932aee302b72759ab68f9a680c3b66a63c5
Murilo Carnelos <muriloc.c@hotmail.com>
2020-04-16 16:17:32 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/murilocarnelos/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/murilocarnelos/workshopmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.murilocarnelos.workshopmongodb.domain.Post;
 import com.murilocarnelos.workshopmongodb.domain.User;
 import com.murilocarnelos.workshopmongodb.dto.UserDTO;
 import com.murilocarnelos.workshopmongodb.services.UserService;
@@ -60,4 +61,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
