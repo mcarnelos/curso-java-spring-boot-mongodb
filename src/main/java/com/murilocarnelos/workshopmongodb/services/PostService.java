e60b7e0b29836064b39e39afc1c9b1d84e99f588
Murilo Carnelos <muriloc.c@hotmail.com>
2020-04-16 17:16:39 -0300

    Consulta simples com query methods

--- src/main/java/com/murilocarnelos/workshopmongodb/services/PostService.java
+++ src/main/java/com/murilocarnelos/workshopmongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.murilocarnelos.workshopmongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,9 @@ public class PostService {
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
 	}
 	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 	
 }
